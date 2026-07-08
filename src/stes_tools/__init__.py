eda3df3f7f598d8efef6c626cf4cee1480935d9d
mankae <mbk.kaelin@gmail.com>
2026-07-08 15:28:31 +0200

    Remove calculate_self_discharge_yearly import from __init__.py

--- src/stes_tools/__init__.py
+++ src/stes_tools/__init__.py
@@ -1,7 +1,7 @@
 from .H2O_prop import density_water, specific_heat_water
 from .cost_functions import CAPEX_STES, OPEX_STES
 from .import_functions import data_import, extract_storage_temperature
-from .loss_simulation import STES, PTES, TTES, simulate_storage_simple, calculate_self_discharge_yearly
+from .loss_simulation import STES, PTES, TTES, simulate_storage_simple
 from .temperature_map import *
 
 __all__ = [
@@ -15,6 +15,5 @@ __all__ = [
     "PTES",
     "TTES",
     "simulate_storage_simple",
-    "calculate_self_discharge_yearly",
     "temperature_map"
 ]
